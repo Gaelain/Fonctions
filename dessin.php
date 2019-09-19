c1baf3a0d81cbef1e9317d67a161a5a8635ca2a6
PaulSIO <paulastru@gmail.com>
2019-09-19 11:57:52 +0200

    fonction dessin

---
+++ dessin.php
@@ -0,0 +1,14 @@
+<?php
+    function dessin(){
+    for ($i = 0; $i<6; $i++)
+    {
+        for($j=0; $j<=$i ;$j++)
+        {
+            echo '*';
+            
+        }
+       echo "<br>";
+    }
+                    }
+    dessin();
+?>
